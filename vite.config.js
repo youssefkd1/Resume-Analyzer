d79f0b691181a15253f8c58fcdd32197c3fec76a
youssefkd1 <yousseffiras555@gmail.com>
2026-05-11 19:51:27 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Resume-Analyzer",
   server: {
     hmr: {
       overlay: false,
